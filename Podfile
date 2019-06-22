99d0d1cc37a814dd900c7294164c9ed062a04391
mht-ryo-chiba <r.chiba@mht-code.com>
2019-06-22 10:31:22 +0900

    プロジェクトファイルの追加

---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'RxSwiftSample' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+  pod 'RxSwift'
+  pod 'RxSwift', '~> 4.4.0'
+  pod 'RxCocoa', '~> 4.4.0'
+
+  # Pods for RxSwiftSample
+
+  target 'RxSwiftSampleTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'RxSwiftSampleUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ RxSwiftSample/RxSwiftViewController.swift
@@ -0,0 +1,48 @@
+//
+//  RxSwiftViewController.swift
+//  RxSwiftSample
+//
+//  Created by 千葉　諒 on 2019/06/22.
+//  Copyright © 2019 chiba. All rights reserved.
+//
+
+import UIKit
+
+class RxSwiftViewController: UIViewController {
+    
+    @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var idTextField: UITextField!
+    @IBOutlet weak var passTextField: UITextField!
+    
+    let enableBtnColor: UIColor = UIColor(red: 96/255, green: 163/255, blue: 255/255, alpha: 1.0)
+    let disenableBtnColor: UIColor = UIColor(red: 173/255, green: 173/255, blue: 173/255, alpha: 1.0)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        initView()
+    }
+    
+    private func initView() {
+        // 初期表示は非活性
+        loginButton.isEnabled = false
+        // ボタンのタイトル色
+        loginButton.setTitleColor(disenableBtnColor,for: UIControl.State.disabled) // タイトルの色
+        // ボタンのボーダー色
+        loginButton.layer.borderColor = disenableBtnColor.cgColor
+        // どのくらい角を丸くするか
+        loginButton.layer.cornerRadius = 4.0
+        // ボーダーのの線の太さ
+        loginButton.layer.borderWidth = 2.0
+    }
+    
+    @IBAction func moveBeforeView(_ sender: Any) {
+        // ViewControllerに繊維
+        let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let viewController: UIViewController = storyboard.instantiateViewController(withIdentifier: "Main")
+        viewController.modalTransitionStyle = UIModalTransitionStyle.flipHorizontal
+        self.present(viewController, animated: true, completion: nil)
+    }
+    
+}
+
